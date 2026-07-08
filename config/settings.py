b71dbc6b90f89e02c52ecf80e5b8fcc02ed5f3c4
DasarathAyer <dasarathayer007@gmail.com>
2026-07-08 15:09:21 +0545

    fix: update STATIC_URL and add STATIC_ROOT for static file handling

--- config/settings.py
+++ config/settings.py
@@ -188,7 +188,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 GDAL_LIBRARY_PATH = config("GDAL_LIBRARY_PATH", default=None)
 
